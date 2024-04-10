6231b4bc3a6928e24707120f66e4e2c6ac20a0c4
FreezyLemon <contact@freezylemon.com>
2024-04-10 15:24:10 +0200

    Move channel_layout_fixed.h to rust (#28)
    * Move channel layout definitions to Rust
    
    * Add channel layouts new in 6.1
    
    * Add new AVChannelLayout consts
    
    * Add new channel layout values to ChannelLayout
    
    * Realign ChannelLayout consts

--- ffmpeg-sys-the-third/src/avutil/mod.rs
+++ ffmpeg-sys-the-third/src/avutil/mod.rs
@@ -12,3 +12,8 @@ pub use self::rational::*;
 
 mod pixfmt;
 pub use self::pixfmt::*;
+
+#[cfg(feature = "ffmpeg_5_1")]
+mod channel_layout;
+#[cfg(feature = "ffmpeg_5_1")]
+pub use self::channel_layout::*;
