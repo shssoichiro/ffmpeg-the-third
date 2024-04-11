a54d0473c38ba6cefc13c9743f14caefaadee9a8
FreezyLemon <contact@freezylemon.com>
2024-04-11 13:57:13 +0200

    Add struct ChannelCustom (<-> AVChannelCustom)

--- src/util/channel_layout/mod.rs
+++ src/util/channel_layout/mod.rs
@@ -2,6 +2,11 @@ mod mask;
 pub use mask::*;
 
 #[cfg(feature = "ffmpeg_5_1")]
+mod channel_custom;
+#[cfg(feature = "ffmpeg_5_1")]
+pub use channel_custom::*;
+
+#[cfg(feature = "ffmpeg_5_1")]
 mod channel;
 #[cfg(feature = "ffmpeg_5_1")]
 pub use channel::*;
