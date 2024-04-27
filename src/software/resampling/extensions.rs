e051fa335a407bde9bebb657b120cad00c129776
FreezyLemon <contact@freezylemon.com>
2024-04-27 14:01:53 +0200

    Add API for swr_alloc_set_opts2

--- src/software/resampling/extensions.rs
+++ src/software/resampling/extensions.rs
@@ -1,8 +1,15 @@
 use super::Context;
 use crate::util::format;
-use crate::{decoder, frame, ChannelLayoutMask, Error};
+use crate::{decoder, frame, Error};
+
+#[cfg(feature = "ffmpeg_5_1")]
+use crate::ChannelLayout;
+
+#[cfg(not(feature = "ffmpeg_7_0"))]
+use crate::ChannelLayoutMask;
 
 impl frame::Audio {
+    #[cfg(not(feature = "ffmpeg_7_0"))]
     #[inline]
     pub fn resampler(
         &self,
@@ -19,9 +26,28 @@ impl frame::Audio {
             rate,
         )
     }
+
+    #[cfg(feature = "ffmpeg_5_1")]
+    #[inline]
+    pub fn resampler2(
+        &self,
+        format: format::Sample,
+        ch_layout: ChannelLayout,
+        rate: u32,
+    ) -> Result<Context, Error> {
+        Context::get2(
+            self.format(),
+            self.ch_layout(),
+            unsafe { (*self.as_ptr()).sample_rate as u32 },
+            format,
+            ch_layout,
+            rate,
+        )
+    }
 }
 
 impl decoder::Audio {
+    #[cfg(not(feature = "ffmpeg_7_0"))]
     #[inline]
     pub fn resampler(
         &self,
@@ -35,6 +61,24 @@ impl decoder::Audio {
             self.rate(),
             format,
             channel_layout,
+            rate,
+        )
+    }
+
+    #[cfg(feature = "ffmpeg_5_1")]
+    #[inline]
+    pub fn resampler2(
+        &self,
+        format: format::Sample,
+        ch_layout: ChannelLayout,
+        rate: u32,
+    ) -> Result<Context, Error> {
+        Context::get2(
+            self.format(),
+            self.ch_layout(),
+            self.rate(),
+            format,
+            ch_layout,
             rate,
         )
     }
