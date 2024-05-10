0aa807e6604c2fc1e6c1e8da85b99673a4e8888f
FreezyLemon <contact@freezylemon.com>
2024-05-10 23:26:57 +0200

    Support platforms where C enums are not uints
    This is a problem on Windows, where signed ints are used.

--- src/util/option/mod.rs
+++ src/util/option/mod.rs
@@ -10,32 +10,32 @@ bitflags! {
     #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
     #[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
     pub struct Type: c_uint {
-        const FLAGS             = AVOptionType::AV_OPT_TYPE_FLAGS.0;
-        const INT               = AVOptionType::AV_OPT_TYPE_INT.0;
-        const INT64             = AVOptionType::AV_OPT_TYPE_INT64.0;
-        const DOUBLE            = AVOptionType::AV_OPT_TYPE_DOUBLE.0;
-        const FLOAT             = AVOptionType::AV_OPT_TYPE_FLOAT.0;
-        const STRING            = AVOptionType::AV_OPT_TYPE_STRING.0;
-        const RATIONAL          = AVOptionType::AV_OPT_TYPE_RATIONAL.0;
-        const BINARY            = AVOptionType::AV_OPT_TYPE_BINARY.0;
-        const DICTIONARY        = AVOptionType::AV_OPT_TYPE_DICT.0;
-        const CONSTANT          = AVOptionType::AV_OPT_TYPE_CONST.0;
+        const FLAGS             = AVOptionType::AV_OPT_TYPE_FLAGS.0 as c_uint;
+        const INT               = AVOptionType::AV_OPT_TYPE_INT.0 as c_uint;
+        const INT64             = AVOptionType::AV_OPT_TYPE_INT64.0 as c_uint;
+        const DOUBLE            = AVOptionType::AV_OPT_TYPE_DOUBLE.0 as c_uint;
+        const FLOAT             = AVOptionType::AV_OPT_TYPE_FLOAT.0 as c_uint;
+        const STRING            = AVOptionType::AV_OPT_TYPE_STRING.0 as c_uint;
+        const RATIONAL          = AVOptionType::AV_OPT_TYPE_RATIONAL.0 as c_uint;
+        const BINARY            = AVOptionType::AV_OPT_TYPE_BINARY.0 as c_uint;
+        const DICTIONARY        = AVOptionType::AV_OPT_TYPE_DICT.0 as c_uint;
+        const CONSTANT          = AVOptionType::AV_OPT_TYPE_CONST.0 as c_uint;
 
-        const IMAGE_SIZE        = AVOptionType::AV_OPT_TYPE_IMAGE_SIZE.0;
-        const PIXEL_FORMAT      = AVOptionType::AV_OPT_TYPE_PIXEL_FMT.0;
-        const SAMPLE_FORMAT     = AVOptionType::AV_OPT_TYPE_SAMPLE_FMT.0;
-        const VIDEO_RATE        = AVOptionType::AV_OPT_TYPE_VIDEO_RATE.0;
-        const DURATION          = AVOptionType::AV_OPT_TYPE_DURATION.0;
-        const COLOR             = AVOptionType::AV_OPT_TYPE_COLOR.0;
+        const IMAGE_SIZE        = AVOptionType::AV_OPT_TYPE_IMAGE_SIZE.0 as c_uint;
+        const PIXEL_FORMAT      = AVOptionType::AV_OPT_TYPE_PIXEL_FMT.0 as c_uint;
+        const SAMPLE_FORMAT     = AVOptionType::AV_OPT_TYPE_SAMPLE_FMT.0 as c_uint;
+        const VIDEO_RATE        = AVOptionType::AV_OPT_TYPE_VIDEO_RATE.0 as c_uint;
+        const DURATION          = AVOptionType::AV_OPT_TYPE_DURATION.0 as c_uint;
+        const COLOR             = AVOptionType::AV_OPT_TYPE_COLOR.0 as c_uint;
         #[cfg(not(feature = "ffmpeg_7_0"))]
-        const CHANNEL_LAYOUT    = AVOptionType::AV_OPT_TYPE_CHANNEL_LAYOUT.0;
+        const CHANNEL_LAYOUT    = AVOptionType::AV_OPT_TYPE_CHANNEL_LAYOUT.0 as c_uint;
         #[cfg(feature = "ffmpeg_5_1")]
-        const CHLAYOUT          = AVOptionType::AV_OPT_TYPE_CHLAYOUT.0;
-        const C_ULONG           = AVOptionType::AV_OPT_TYPE_UINT64.0;
-        const BOOL              = AVOptionType::AV_OPT_TYPE_BOOL.0;
+        const CHLAYOUT          = AVOptionType::AV_OPT_TYPE_CHLAYOUT.0 as c_uint;
+        const C_ULONG           = AVOptionType::AV_OPT_TYPE_UINT64.0 as c_uint;
+        const BOOL              = AVOptionType::AV_OPT_TYPE_BOOL.0 as c_uint;
 
         #[cfg(feature = "ffmpeg_7_0")]
-        const FLAG_ARRAY        = AVOptionType::AV_OPT_TYPE_FLAG_ARRAY.0;
+        const FLAG_ARRAY        = AVOptionType::AV_OPT_TYPE_FLAG_ARRAY.0 as c_uint;
     }
 }
 
@@ -47,12 +47,13 @@ impl Default for Type {
 
 impl From<AVOptionType> for Type {
     fn from(value: AVOptionType) -> Self {
-        Self::from_bits_retain(value.0)
+        Self::from_bits_retain(value.0 as c_uint)
     }
 }
 
 impl From<Type> for AVOptionType {
     fn from(value: Type) -> AVOptionType {
-        AVOptionType(value.bits())
+        // cast to whichever type the C enum uses
+        AVOptionType(value.bits() as _)
     }
 }
