3ada310e4f2e74104610855eaf6c8a526f692d35
FreezyLemon <contact@freezylemon.com>
2024-06-16 14:45:22 +0200

    Add rustc-check-cfg in main crate
    The sys build script sets an environment variable for every
    feature that needs to be added to rustc-check-cfg.

--- build.rs
+++ build.rs
@@ -2,7 +2,14 @@ use std::env;
 
 fn main() {
     for (name, _value) in env::vars() {
-        if name.starts_with("DEP_FFMPEG_") {
+        println!("{name}");
+
+        if name.starts_with("DEP_FFMPEG_CHECK_") {
+            println!(
+                r#"cargo:rustc-check-cfg=cfg(feature, values("{}"))"#,
+                name["DEP_FFMPEG_CHECK_".len()..name.len()].to_lowercase()
+            );
+        } else if name.starts_with("DEP_FFMPEG_") {
             println!(
                 r#"cargo:rustc-cfg=feature="{}""#,
                 name["DEP_FFMPEG_".len()..name.len()].to_lowercase()
