b4c2d51a90df937f6ae1df1b00e04ec35bc4ba35
FreezyLemon <contact@freezylemon.com>
2025-08-30 18:31:42 +0200

    allow fn pointer comparison lint
    this throws warnings on all PartialEq derives
    for structs with fn pointers. For now I'd like to
    keep compatibility and just allow the lint, but
    this should probably be revisited at some point.

--- ffmpeg-sys-the-third/src/lib.rs
+++ ffmpeg-sys-the-third/src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(unpredictable_function_pointer_comparisons)]
 #![allow(clippy::approx_constant)]
 #![allow(clippy::missing_safety_doc)]
 #![allow(clippy::redundant_static_lifetimes)]
